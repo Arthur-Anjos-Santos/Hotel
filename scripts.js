87ba6b99da630aa8ae9e1e2dd16c94a9380e752b
Arthur Anjos Santos <62223796+Arthur-Anjos-Santos@users.noreply.github.com>
2024-06-06 14:01:47 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -19,11 +19,35 @@ function login(event) {
 
 function register(event) {
     event.preventDefault();
-    // Handle registration logic here (e.g., save user data)
-    alert("Registration successful! Please log in.");
+    alert("Registrado com sucesso! Faça seu login.");
     showLogin();
 }
 
 document.addEventListener('DOMContentLoaded', (event) => {
     showLogin();
 });
+
+function maskCPF(input) {
+    let value = input.value;
+    value = value.replace(/\D/g, ""); 
+    value = value.replace(/(\d{3})(\d)/, "$1.$2");
+    value = value.replace(/(\d{3})(\d)/, "$1.$2");
+    value = value.replace(/(\d{3})(\d{1,2})$/, "$1-$2");
+    input.value = value;
+}
+
+function maskDate(input) {
+    let value = input.value;
+    value = value.replace(/\D/g, "");
+    value = value.replace(/(\d{2})(\d)/, "$1/$2");
+    value = value.replace(/(\d{2})(\d)/, "$1/$2");
+    input.value = value;
+}
+
+function calculateStay(event) {
+    event.preventDefault();
+    const days = document.getElementById('bookingDays').value;
+    const costPerDay = 150;
+    const totalCost = days * costPerDay;
+    document.getElementById('costDisplay').innerText = `Total de custo para ${days} dia(s) é R$${totalCost}`;
+}
